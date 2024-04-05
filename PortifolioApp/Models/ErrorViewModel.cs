c7ac0375fd06e41759dbca48f9f20531e7c88c5a
mizaeldouglas <mizaeldouglas2@gmail.com>
2024-04-05 18:55:58 -0300

    Initial commit

---
+++ PortifolioApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PortifolioApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
